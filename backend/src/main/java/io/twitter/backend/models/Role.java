614eac47a29866b3427df68ee8bcebd5d85fc2be
Angel J. Haro <haro.j.angel@gmail.com>
2024-05-24 01:43:50 -0700

    model: role

---
+++ backend/src/main/java/io/twitter/backend/models/Role.java
@@ -0,0 +1,35 @@
+package io.twitter.backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "roles")
+public class Role {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "role_id")
+  private Long roleId;
+
+  // AUTH stuff
+  private String authority;
+
+  @Override
+  public String toString() {
+    return "Role [roleId=" + roleId + ", authority=" + authority + "]";
+  }
+
+}
