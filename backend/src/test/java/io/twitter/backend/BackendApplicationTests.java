5f752f940598f1f2858570eb394ca46c617e16e1
Angel J. Haro <haro.j.angel@gmail.com>
2024-05-24 01:11:46 -0700

    init

---
+++ backend/src/test/java/io/twitter/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.twitter.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
