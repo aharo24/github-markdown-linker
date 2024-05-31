9bbf19652a69f83422f2ec7c57a62104effe9943
Angel J. Haro <haro.j.angel@gmail.com>
2024-05-30 23:49:46 -0700

    model: role

--- backend/src/main/java/io/twitter/backend/models/Role.java
+++ backend/src/main/java/io/twitter/backend/models/Role.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 public class Role {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "role_id")
   private Long roleId;
 
