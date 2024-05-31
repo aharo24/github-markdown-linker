08fb745aa62f063f26f46c9c45682b303948ed09
Angel J. Haro <haro.j.angel@gmail.com>
2024-05-30 23:49:22 -0700

    CORS

---
+++ backend/src/main/java/io/twitter/backend/config/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package io.twitter.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+
+    // decapricated: http.csrf().disable();
+    // decapricated: http.authorizeRequests().anyRequest().permitAll();
+    return http.build();
+  }
+}
