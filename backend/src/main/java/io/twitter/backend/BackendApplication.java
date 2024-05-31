1642898ea0d91d1732132352c11feb58cc24582b
Angel J. Haro <haro.j.angel@gmail.com>
2024-05-30 23:50:19 -0700

    Application

--- backend/src/main/java/io/twitter/backend/BackendApplication.java
+++ backend/src/main/java/io/twitter/backend/BackendApplication.java
@@ -1,13 +1,42 @@
 package io.twitter.backend;
 
+import java.util.HashSet;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.twitter.backend.models.ApplicationUser;
+import io.twitter.backend.models.Role;
+import io.twitter.backend.repositories.RoleRepository;
+import io.twitter.backend.repositories.UserRepository;
+import io.twitter.backend.services.UserService;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+  }
 
+  @Bean
+  CommandLineRunner run(RoleRepository roleRepo, UserService userService) {
+    return args -> {
+      roleRepo.save(new Role(1L, "USER"));
+      ApplicationUser u = new ApplicationUser();
+      u.setFirstName("aharo");
+      u.setLastName("J");
+      // userService.registerUser(u);
+      /*
+       * ApplicationUser u = new ApplicationUser();
+       * u.setFirtName("Angel");
+       * u.setLastName("Haro");
+       * HashSet<Role> roles = new HashSet<>();
+       * roles.add(roleRepo.findRoleByAuthority("USER").get());
+       * u.setAuthorities(roles);
+       * userRepo.save(u);
+       */
+    };
+  }
 }
